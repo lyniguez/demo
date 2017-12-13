3cf9f3fda083239ad01a4e9cb1b0a23e034c5d9c
lyniguez@gmail.com <lyniguez@Gmail.com>
2017-12-12 18:51:29 -0800

    second

---
+++ new2.py
@@ -0,0 +1,13 @@
+git add this
+
+git checkout -b "name branch"
+	use git checkout to switch to another branch and/or create new branch
+	
+git branch second_branch
+#creates a new branch
+
+git checkout
+#switches to new branch
+
+git checkout master
+#goes back to master
